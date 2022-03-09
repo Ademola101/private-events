7b3bbd1844b7781910eb825b37e3b79de19ab227
Ademola <ademolaogunmokun@yahoo.com>
2022-03-09 13:33:46 +0100

    delete c code

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ApplicationRecord  
   belongs_to :attendee, :class_name => "User"
   belongs_to :attended_event, :class_name => "Event"
+  accepts_nested_attributes_for :attendee
+  validates :attendee, length: {minimum:1}
 end
